f0aeaa87e04816c9360c2f7cb95556853c8bd6aa
duanhuiyang <huiyang.duan@pqitech.com>
2024-03-20 18:08:19 +0800

    增加Comparable接口

--- include/global.h
+++ include/global.h
@@ -1,5 +1,36 @@
 #ifndef GLOBAL
 #define GLOBAL
-using ElementType = int;
+
+template <class T>
+class Comparable
+{
+public:
+    Comparable() = default;
+    ~Comparable() = default;
+    virtual int CampareTo(Comparable &item) = 0;
+
+public:
+    T _data;
+};
+
+class element : protected Comparable<int>
+{
+    inline int CampareTo(Comparable &item) override
+    {
+        if (this->_data < item._data)
+        {
+            return -1;
+        }
+        else if (this->_data == item._data)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+};
+using ElementType = element;
 
 #endif /* GLOBAL */
